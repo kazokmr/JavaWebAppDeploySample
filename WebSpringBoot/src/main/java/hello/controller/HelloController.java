fc375726cea6c9ea1ffc07359c6adaec3cb4ba9e
Kazuhiko Okumura <engkokmr@gmail.com>
2019-01-22 17:28:26 +0900

    Create Spring Boot Web Application project

---
+++ WebSpringBoot/src/main/java/hello/controller/HelloController.java
@@ -0,0 +1,14 @@
+package hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+  @GetMapping("/")
+  public String index(Model model) {
+    model.addAttribute("message", "Greetings from Spring Boot!");
+    return "index";
+  }
+}
